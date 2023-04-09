51706b6780dbd4144485ffa484db5893f5c60826
Emir490 <chido_emir@hotmail.com>
2023-04-08 19:12:39 -0700

    Capturing video every 30 frames

--- src/components/holisticComponent.tsx
+++ src/components/holisticComponent.tsx
@@ -1,12 +1,58 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Camera } from '@mediapipe/camera_utils';
 import { drawConnectors, drawLandmarks } from '@mediapipe/drawing_utils';
 import { Holistic, Results, FACEMESH_TESSELATION, POSE_CONNECTIONS, HAND_CONNECTIONS, } from '@mediapipe/holistic';
 import Webcam from 'react-webcam';
 
+const sequenceLength = 30; // You can set the desired sequence length here.
+
 const HolisticComponent: React.FC = () => {
+  const [isCollecting, setIsCollecting] = useState<boolean>(false);
+
   const webcamRef = useRef<Webcam>(null);
   const canvasRef = useRef<HTMLCanvasElement>(null);
+  const collectingRef = useRef<boolean>(false);
+  const framesRef = useRef<number[][]>([]); // Use useRef to store frames
+
+  useEffect(() => {
+    collectingRef.current = isCollecting;
+  }, [isCollecting]);
+
+  const onFrame = (results: Results) => {
+    if (collectingRef.current) {
+      const keypoints = extractKeyPoints(results);
+      
+      framesRef.current.push(keypoints);
+      
+      if (framesRef.current.length === sequenceLength) {
+        sendFramesToServer();
+        framesRef.current = []
+      }
+    }
+  }
+
+  const sendFramesToServer = async () => {
+    console.log("Action", framesRef.current);
+  
+    // Disable collecting while waiting
+    setIsCollecting(false);
+    
+    if (webcamRef.current && webcamRef.current.video) {
+      webcamRef.current.video.pause();
+    }
+    console.log('Collecting Frames...');
+  
+    // Wait for 2 seconds
+    setTimeout(() => {
+      // Continue collecting frames after the wait
+      if (webcamRef.current && webcamRef.current.video) {
+        webcamRef.current.video.play();
+      }
+
+      setIsCollecting(true);
+    }, 2000);
+  };
+  
 
   const mediapipeDetection = async (videoElement: HTMLVideoElement, canvasElement: HTMLCanvasElement) => {
     const holistic = new Holistic({
@@ -24,13 +70,12 @@ const HolisticComponent: React.FC = () => {
     holistic.onResults((results) => {
       drawStyledLandmarks(canvasElement, results, videoElement);
 
-      const keypoints = extractKeyPoints(results);
-      console.log(keypoints);
+      onFrame(results);
     });
 
     const camera = new Camera(videoElement, {
       onFrame: async () => {
-        await holistic.send({image: videoElement});
+        await holistic.send({ image: videoElement });
       },
       width: 640,
       height: 480
@@ -43,21 +88,21 @@ const HolisticComponent: React.FC = () => {
     const pose = results.poseLandmarks
       ? results.poseLandmarks.map((res) => [res.x, res.y, res.z, res.visibility]).flat()
       : Array(33 * 4).fill(0);
-  
+
     const face = results.faceLandmarks
       ? results.faceLandmarks.map((res) => [res.x, res.y, res.z]).flat()
       : Array(468 * 3).fill(0);
-  
+
     const lh = results.leftHandLandmarks
       ? results.leftHandLandmarks.map((res) => [res.x, res.y, res.z]).flat()
       : Array(21 * 3).fill(0);
-  
+
     const rh = results.rightHandLandmarks
       ? results.rightHandLandmarks.map((res) => [res.x, res.y, res.z]).flat()
       : Array(21 * 3).fill(0);
-  
+
     return [...pose, ...face, ...lh, ...rh];
-  }  
+  }
 
   const drawStyledLandmarks = (canvas: HTMLCanvasElement | null, results: Results, videoElement: HTMLVideoElement | null) => {
 
@@ -133,7 +178,7 @@ const HolisticComponent: React.FC = () => {
 
   return (
     <div>
-       <Webcam
+      <Webcam
         ref={webcamRef}
         width={640}
         height={480}
@@ -141,8 +186,9 @@ const HolisticComponent: React.FC = () => {
         style={{ display: "none" }}
       />
       <canvas ref={canvasRef} width="1080" height="720"></canvas>
+      <button className='bg-indigo-800 p-3 text-white uppercase font-bold rounded-md hover:bg-indigo-700 transition-colors my-5' onClick={() => setIsCollecting(!isCollecting)}>Start Collecting Frames</button>
     </div>
-  );  
+  );
 }
 
 export default HolisticComponent
