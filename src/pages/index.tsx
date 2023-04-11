3b4d6684912d90ef69f471344a65f03736e6f9f3
ValdezFOmar <omarantoniovaldezf2@gmail.com>
2023-04-10 18:42:49 -0700

    UI

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,6 @@ import HolisticComponent from "@/components/holisticComponent";
 import Layout from "@/components/layout";
 import { useState } from "react";
 import CategoriesList from "@/components/categoriesList";
-import ActionsList from "@/components/actionsList";
 
 export default function Home() {
   const [toggle, setToggle] = useState(true);
@@ -13,7 +12,7 @@ export default function Home() {
       <div className="container mx-auto">
         {/* <Video /> */}
         {/* {toggle && <HolisticComponent />} */}
-        <button className="bg-indigo-800 p-3 text-white uppercase font-bold rounded-md hover:bg-indigo-700 transition-colors" onClick={() => setToggle(!toggle)}>Toggle</button>
+        {/* <button className="bg-indigo-800 p-3 text-white uppercase font-bold rounded-md hover:bg-indigo-700 transition-colors" onClick={() => setToggle(!toggle)}>Toggle</button> */}
       </div>
     </Layout>
   );
