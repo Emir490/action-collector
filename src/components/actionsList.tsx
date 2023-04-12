ee7a47babd4b4ee8705d18d78c25b09284859d4d
ValdezFOmar <omarantoniovaldezf2@gmail.com>
2023-04-11 17:46:05 -0700

    Responsibe for mobile

--- src/components/actionsList.tsx
+++ src/components/actionsList.tsx
@@ -16,9 +16,9 @@ const ActionsList = ({ category }: ActionsListProps) => {
 
     //Returns all the actions in buttons
     return (
-        <div className="flex-1 grid grid-cols-6 gap-2">
+        <div className="grid grid-cols-4 md:grid-cols-6 lg:grid-cols-8 gap-2">
             {actionsArray.map((action) => (
-                <Link href={`${action}`} className="text-white p-3 bg-indigo-800 hover:bg-indigo-600 transition-colors rounded first-letter:uppercase"
+                <Link href={`${action}`} className=" text-center text-white p-3 bg-indigo-800 hover:bg-indigo-600 hover:font-semibold transition-colors rounded first-letter:uppercase"
                     onClick={() => { console.log(`Action: ${action}`); }} key={action}>
                     {action}
                 </Link>
--- src/components/categoriesList.tsx
+++ src/components/categoriesList.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { getAllActions, IActions } from "@/helpers";
 import ActionsList from "./actionsList";
 
@@ -16,27 +16,56 @@ const categories = extractCategories(actions);
 
 const CategoriesList: React.FC = () => {
   const [selectedOption, setSelectedOption] = useState(categories[0]);
+  const [isMobile, setIsMobile] = useState(false);
 
-  // Gets the categorie of the clicked item
-  const handleOptionChange = (category: string) => {
+  // Gets the categorie of the clicked item (Butttons)
+  const handleClickButton = (category: string) => {
     setSelectedOption(category);
     console.log(`Category: ${category}`);
   };
 
-  // TODO: Implement functionality for buttons
-  // TODO: Better styling (Tailwind CSS)
+  // Gets the category of selected item (DropList)
+  const handleOptionChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setSelectedOption(event.target.value)
+  }
+
+  // Determine screen size
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 768); // Breakpoint for mobile screens
+    };
+    handleResize(); // Initial detection
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   // Returns all the categories in buttons
   return (
-    <div className="container mx-auto flex gap-x-6 mt-5">
-      <div className="flex flex-col">
-        {categories.map((category) => (
-          <button className="p-2 bg-purple-900 hover:bg-purple-700 text-white ml-3 rounded w-full mb-2 transition-colors"
-            onClick={() => { handleOptionChange(category) }} key={category}>
-            {category}
-          </button>
-        ))}
+    <div className="flex flex-col md:flex-row px-3">
+      {isMobile ? ( // Display for mobile
+        <div className="m-3">
+          <select className="font-semibold text- p-2 bg-purple-900 text-white rounded w-full transition-colors"
+                  value={selectedOption} onChange={handleOptionChange}>
+            {categories.map((category) => (
+              <option value={category}>
+                {category}
+              </option>
+            ))}
+          </select>
+        </div>
+      ) : ( // Display for larger screens
+        <div className="basis-1/6 flex md:flex-col md:gap-y-2 m-3">
+          {categories.map((category) => (
+            <button className="font-semibold hover:font-bold p-2 bg-purple-900 hover:bg-purple-700 text-white rounded w-full transition-colors"
+              onClick={() => { handleClickButton(category) }} key={category}>
+              {category}
+            </button>
+          ))}
+        </div>
+      )}
+      <div className="md:basis-5/6 m-3">
+        <ActionsList category={selectedOption} />
       </div>
-      <ActionsList category={selectedOption}/>
     </div>
   )
 }
