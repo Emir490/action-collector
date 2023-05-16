8a7597b64b7ce471b30f58cc29da76c1c6b3cbfb
Emir490 <chido_emir@hotmail.com>
2023-05-15 17:43:23 -0700

    Responsive design

--- src/components/gestureRecognition.tsx
+++ src/components/gestureRecognition.tsx
@@ -1,52 +1,85 @@
-import React, { useRef, useEffect } from 'react';
-import Webcam from 'react-webcam';
+import React, { useRef, useEffect, useState, ReactNode, useCallback } from "react";
+import Webcam from "react-webcam";
 
 const GestureRecognition: React.FC = () => {
+  const [gestureName, setGestureName] = useState<string>("");
+  const [isCamera, setIsCamera] = useState(false);
+
   const webcamRef = useRef<Webcam>(null);
-  let lastVideoTime = -1;
-
-  const mediapipeRecognition = async (videoElement: HTMLVideoElement) => {
-    const { FilesetResolver, GestureRecognizer } = await import('@mediapipe/tasks-vision');
-    const { Camera } = await import('@mediapipe/camera_utils');
-    const vision = await FilesetResolver.forVisionTasks("https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@latest/wasm");
-
-    const gestureRecognizer = await GestureRecognizer.createFromOptions(vision, { baseOptions: {modelAssetPath: "../../asl_model/gesture_recognizer.task"}, numHands: 2});
-
-    gestureRecognizer.setOptions({ runningMode: "VIDEO" });
-
-    const camera = new Camera(videoElement, {
-      onFrame: async () => {
-        lastVideoTime = videoElement.currentTime;
-        const results = gestureRecognizer.recognizeForVideo(videoElement, Date.now());
-        const gestures = results.gestures[0];
-        if (gestures) {
-          console.log(gestures[0].categoryName);
-        }
-      },
-      width: 640,
-      height: 480
-    });
-    camera.start();
-  }
+  
+  const toggleCamera = () => setIsCamera(!isCamera);
 
   useEffect(() => {
+    const mediapipeRecognition = async (videoElement: HTMLVideoElement) => {
+      const { FilesetResolver, GestureRecognizer } = await import(
+        "@mediapipe/tasks-vision"
+      );
+      const { Camera } = await import("@mediapipe/camera_utils");
+      const vision = await FilesetResolver.forVisionTasks(
+        "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@latest/wasm"
+      );
+  
+      const gestureRecognizer = await GestureRecognizer.createFromOptions(
+        vision,
+        {
+          baseOptions: {
+            modelAssetPath: "../../asl_model/gesture_recognizer.task",
+          }
+        }
+      );
+  
+      gestureRecognizer.setOptions({ runningMode: "VIDEO" });
+  
+      const camera = new Camera(videoElement, {
+        onFrame: async () => {
+          const results = gestureRecognizer.recognizeForVideo(
+            videoElement,
+            Date.now()
+          );
+          const gestures = results.gestures[0];
+          if (gestures) {
+            console.log(gestures[0].categoryName)
+            setGestureName(gestures[0].categoryName);
+          }
+        },
+        width: 640,
+        height: 480,
+      });
+      camera.start();
+    };
+
     const handleMediapipe = async () => {
+      console.log(webcamRef.current)
       if (webcamRef.current) {
         const videoElement = webcamRef.current.video as HTMLVideoElement;
         mediapipeRecognition(videoElement);
       }
     }
+
     handleMediapipe();
-  }, [webcamRef]);
+  }, [webcamRef, isCamera]);
 
   return (
-    <div>
-      <Webcam
-        ref={webcamRef}
-        width={640}
-        height={480}
-        screenshotFormat="image/jpeg"
-      />
+    <div className={`${isCamera ? 'relative w-full h-screen' : ''}`}>
+      <button
+        className="p-3 bg-indigo-700 hover:bg-indigo-800 transition-colors text-white font-bold uppercase rounded-md"
+        onClick={() => toggleCamera()}
+      >
+        {isCamera ? "Apagar" : "Encender"}
+      </button>
+      {isCamera && (
+        <>
+          <Webcam
+            mirrored
+            ref={webcamRef}
+            className="w-full h-full object-cover"
+            screenshotFormat="image/jpeg"
+          />
+          <p className="absolute top-10 left-0 m-4 text-5xl text-white font-bold uppercase">
+            {gestureName}
+          </p>
+        </>
+      )}
     </div>
   );
 };
---
+++ src/pages/menu.tsx
@@ -0,0 +1,13 @@
+import CategoriesList from '@/components/categoriesList'
+import Layout from '@/components/layout'
+import React from 'react'
+
+const Menu = () => {
+  return (
+    <Layout>
+      <CategoriesList />
+    </Layout>
+  )
+}
+
+export default Menu
