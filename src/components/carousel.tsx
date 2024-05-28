e86d79411004518736724d14fe44302403045f0e
Emir490 <chido_emir@hotmail.com>
2024-05-28 13:29:09 -0700

    Landpage updated

--- src/components/carousel.tsx
+++ src/components/carousel.tsx
@@ -1,26 +1,10 @@
 import { useState } from "react";
 import { signs } from "@/helpers";
-import Slider from "react-slick";
 import Image from "next/image";
 import React from "react";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
 
 const Carousel: React.FC = () => {
   const [isOpen, setIsOpen] = useState(false);
-
-  const settings = {
-    dots: false,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows: true,
-    vertical: true,
-    verticalSwiping: true,
-    nextArrow: <NextArrow />,
-    prevArrow: <PrevArrow />,
-  };
 
   const toggleCarousel = () => {
     setIsOpen(!isOpen);
@@ -30,55 +14,29 @@ const Carousel: React.FC = () => {
     <>
       <button
         onClick={toggleCarousel}
-        className="fixed top-20 right-4 z-50 p-3 bg-blue-600 hover:bg-blue-700 text-white rounded-full shadow-lg transition-all duration-300 ease-in-out"
+        className="fixed top-20 right-4 z-50 p-3 bg-orange-400 hover:bg-orange-500 text-white rounded-full shadow-lg transition-all duration-300 ease-in-out"
       >
         {isOpen ? "✖" : "☰"}
       </button>
       {isOpen && (
-        <div className="fixed top-24 right-4 w-80 h-1/4 bg-white shadow-xl rounded-lg z-40 overflow-hidden transition-transform transform origin-top-right">
-          <Slider {...settings} className="h-full">
+        <div className="fixed top-24 right-4 w-64 h-3/4 sm:w-80 bg-white shadow-xl rounded-lg z-40 overflow-y-auto">
+          <div className="space-y-4 p-4">
             {signs.map((sign, index) => (
-              <div key={index} className="flex flex-col items-center justify-center p-4 h-full">
-                <div className="w-full h-full flex justify-center items-center">
-                  <Image
-                    src={sign?.img}
-                    alt={`${sign?.name} Seña`}
-                    width={300}
-                    height={300}
-                    className="rounded-lg shadow-lg object-cover w-full h-full"
-                  />
-                </div>
+              <div key={index} className="flex flex-col items-center justify-center p-4 rounded-lg shadow-lg">
+                <Image
+                  src={sign?.img}
+                  alt={`${sign?.name} Seña`}
+                  width={300}
+                  height={300}
+                  className="rounded-lg object-contain"
+                />
                 <p className="mt-4 text-lg font-semibold text-gray-800 text-center">{sign.name}</p>
               </div>
             ))}
-          </Slider>
+          </div>
         </div>
       )}
     </>
-  );
-};
-
-const NextArrow = (props: any) => {
-  const { onClick } = props;
-  return (
-    <div
-      className="absolute top-1/2 right-0 transform -translate-y-1/2 p-2 bg-blue-600 hover:bg-blue-700 text-white rounded-full shadow-lg cursor-pointer"
-      onClick={onClick}
-    >
-      ↓
-    </div>
-  );
-};
-
-const PrevArrow = (props: any) => {
-  const { onClick } = props;
-  return (
-    <div
-      className="absolute top-1/2 left-0 transform -translate-y-1/2 p-2 bg-blue-600 hover:bg-blue-700 text-white rounded-full shadow-lg cursor-pointer"
-      onClick={onClick}
-    >
-      ↑
-    </div>
   );
 };
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,24 @@
 import Layout from "@/components/layout";
 import GestureRecognition from "@/components/gestureRecognition";
 import Carousel from "@/components/carousel";
+import Image from "next/image";
+import Icon from "@/images/icon.png";
 
 export default function Home() {
   return (
     <Layout>
-      <GestureRecognition/>
+      <section className="my-8 px-4 sm:px-8 lg:px-52">
+        <div className="flex flex-col items-center sm:flex-row sm:items-start mb-6">
+          <Image src={Icon} alt="Icono SignAI" width={64} height={64} className="mb-4 sm:mb-0 sm:mr-4" />
+          <p className="text-lg sm:text-xl text-justify">
+            Sign AI es una aplicación que permite al usuario manejar el lenguaje de señas mexicano con el fin de facilitar su comunicación o aprendizaje. El proyecto se enfoca en abordar la barrera de comunicación que enfrentan las personas con discapacidad auditiva al desarrollar un sistema de reconocimiento del lenguaje de señas mexicano.
+          </p>
+        </div>
+        <p className="text-lg sm:text-xl pb-10 text-justify">
+          A continuación se presenta un módulo donde puede realizar y capturar señas, solo tiene que presionar el botón de “Encender” la cámara y comenzar a realizar señas libremente. Si es que quiere borrar la seña que ha capturado, presione el botón de “Borrar” la seña, y si es que quiere borrar toda la cadena, presione el botón de “Reiniciar” la captura.
+        </p>
+      </section>
+      <GestureRecognition />
       <Carousel />
     </Layout>
   );
